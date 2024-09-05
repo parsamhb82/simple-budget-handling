2dbc0c0caf697d2a684d58f810542c64b1fe5360
parsamhb82 <parsa1382mohebian@gmail.com>
2024-09-05 12:18:43 +0330

    final changes

--- budget_handling/transaction/serilizers.py
+++ budget_handling/transaction/serilizers.py
@@ -5,3 +5,8 @@ class CreatTransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
         fields = ('id', 'name', 'amount', 'description')
+    
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = '__all__'
--- budget_handling/transaction/views.py
+++ budget_handling/transaction/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Transaction
-from .serilizers import CreatTransactionSerializer
+from .serilizers import CreatTransactionSerializer, TransactionSerializer
 from rest_framework.views import APIView
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 from rest_framework.response import Response
@@ -18,6 +18,12 @@ class CreatTransaction(CreateAPIView):
             return Response({'error': 'Not enough budget'}, status=status.HTTP_400_BAD_REQUEST)
         user.userbudget.budget += float(amount)
         serializer.save(user=self.request.user)
+class ViewTransactions(ListAPIView):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+    permission_classes = [IsAuthenticated]
+    def get_queryset(self):
+        return Transaction.objects.filter(user=self.request.user)
         
 
 
