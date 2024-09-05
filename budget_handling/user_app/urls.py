f886b94a42fa5f20861a53130911e2afab50474b
parsamhb82 <parsa1382mohebian@gmail.com>
2024-09-05 12:33:51 +0330

    urls fixed

--- budget_handling/user_app/urls.py
+++ budget_handling/user_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import Login, Refresh, UserRegisteration
 
 urlpatterns = [
-    path("/login", Login.as_view(), name="login"),
-    path("/refresh", Refresh.as_view(), name="refresh"),
-    path("/register", UserRegisteration.as_view(), name="register"),
+    path("login/", Login.as_view(), name="login"),
+    path("refresh/", Refresh.as_view(), name="refresh"),
+    path("register/", UserRegisteration.as_view(), name="register"),
 ]
